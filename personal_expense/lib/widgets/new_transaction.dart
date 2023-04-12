ee1d8b60606ef05cb9460702475e92c99782b5c8
Quan Do <minhkwan.it@gmail.com>
2023-04-12 14:28:43 +0700

    Final Product

--- personal_expense/lib/widgets/new_transaction.dart
+++ personal_expense/lib/widgets/new_transaction.dart
@@ -43,40 +43,47 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Container(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            TextField(
-              decoration: const InputDecoration(labelText: 'Title'),
-              controller: titleController,
-              onSubmitted: (_) => submitData(),
-            ),
-            TextField(
-              decoration: const InputDecoration(labelText: 'Amount'),
-              controller: amountController,
-              keyboardType: TextInputType.number,
-              onSubmitted: (_) => submitData(),
-            ),
-            TextButton(
-              style: TextButton.styleFrom(
-                foregroundColor: Theme.of(context).primaryColor,
+    return SingleChildScrollView(
+      child: Card(
+        child: Container(
+          padding: EdgeInsets.only(
+            top: 10,
+            right: 10,
+            left: 10,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 10,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              TextField(
+                decoration: const InputDecoration(labelText: 'Title'),
+                controller: titleController,
+                onSubmitted: (_) => submitData(),
               ),
-              onPressed: submitData,
-              child: const Text('Add Transaction'),
-            ),
-            Row(
-              children: [
-                Text(pickedDate,
-                    style: Theme.of(context).textTheme.titleMedium),
-                IconButton(
-                    onPressed: pickDate,
-                    icon: const Icon(Icons.edit_calendar_rounded)),
-              ],
-            )
-          ],
+              TextField(
+                decoration: const InputDecoration(labelText: 'Amount'),
+                controller: amountController,
+                keyboardType: TextInputType.number,
+                onSubmitted: (_) => submitData(),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  foregroundColor: Theme.of(context).primaryColor,
+                ),
+                onPressed: submitData,
+                child: const Text('Add Transaction'),
+              ),
+              Row(
+                children: [
+                  Text(pickedDate,
+                      style: Theme.of(context).textTheme.titleMedium),
+                  IconButton(
+                      onPressed: pickDate,
+                      icon: const Icon(Icons.edit_calendar_rounded)),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
